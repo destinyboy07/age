8ef040f244094872e4c7d962b93545e400ece072
destinyboy07 <144793286+destinyboy07@users.noreply.github.com>
2023-10-12 10:19:41 +0100

    Add files via upload

---
+++ jsass.js
@@ -0,0 +1,8 @@
+var age = prompt ("how old are you")
+var days = age * 365.25
+var weeks = age * 52
+var hours = weeks * 168
+var seconds = hours * 86400
+var ageInhundred = 1 + age
+alert ("User you have lived for " + days +" days " + weeks + " weeks " + hours + " hours " + seconds + " secs.")
+alert ("you will be " + ageInhundred + " in hundred years.")
